af131c2d1c928b5a31ca92e078db8ade1f94c8f5
RubyZHY <6130116206@email.ncu.edu.cn>
2018-12-01 18:59:55 +0800

    张红玉 增加了登录

--- src/controller/loginServlet.java
+++ src/controller/loginServlet.java
@@ -1,15 +1,15 @@
 package controller;
 
+import dao.getConn;
 import dao.getUser;
 import model.user;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+import javax.swing.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @auther: Liu Zedi.
@@ -19,7 +19,7 @@ import java.io.IOException;
  */
 @WebServlet(name = "loginServlet")
 public class loginServlet extends HttpServlet {
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
     public loginServlet() {
         super();
@@ -27,19 +27,34 @@ public class loginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
-        response.setCharacterEncoding("utf-8");
-        String username=request.getParameter("username");
-        String password=request.getParameter("password");
-        user user=new getUser().getUser(username);
-        if (user!=null&&user.getUsername().equals(password)){
-            Cookie userCookie=new Cookie(user.getId(),user.getUsername());
-            userCookie.setMaxAge(60*60*24*7);
-            response.addCookie(userCookie);
+        response.setCharacterEncoding("gb2312");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        getUser u=new getUser();
+        String name=u.getUser(username).getPassword();
+        String code=request.getParameter("code");
+        HttpSession session=request.getSession();
+        String randStr=(String) session.getAttribute("randStr");
+        PrintWriter out=response.getWriter();
+        if(!code.equals(randStr)){
+            out.println("验证码有误");
+        }
+        else{
+            if (name!= null &&name.equals(password)){
+                HttpSession sessionUser=request.getSession();
+                sessionUser.setAttribute("username",u.getUser(name));
+                response.sendRedirect("../../index.jsp");
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"wrong");
+                response.sendRedirect("../../client/login.html");
+            }
         }
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
+
 }
